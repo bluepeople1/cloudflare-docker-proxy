552ac56268b4946ed4db301e41cd04ff27053e75
bluepeople1 <85560688@qq.com>
2024-09-13 18:29:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zhihuizhaocai.com": dockerHub,
+  "quay.zhihuizhaocai.com": "https://quay.io",
+  "gcr.zhihuizhaocai.com": "https://gcr.io",
+  "k8s-gcr.zhihuizhaocai.com": "https://k8s.gcr.io",
+  "k8s.zhihuizhaocai.com": "https://registry.k8s.io",
+  "ghcr.zhihuizhaocai.com": "https://ghcr.io",
+  "cloudsmith.zhihuizhaocai.com": "https://docker.cloudsmith.io",
+  "ecr.zhihuizhaocai.com": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zhihuizhaocai.com": dockerHub,
 };
 
 function routeByHosts(host) {
